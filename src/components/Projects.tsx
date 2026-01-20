9a941195ba7e8cf78c726d7cee95abd99c6edc24
Dinal <sandarudinal2006@gmail.com>
2026-01-20 22:14:10 +0530

    Commit for 20/1

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,12 +7,17 @@ import { motion } from 'motion/react';
 /**
  * custom modules
  */
-import { fadeUp, staggerContainer } from '@/lib/animations';
+import { staggerContainer } from '@/lib/animations';
+
+/**
+ *  Components
+ */
+import { SectionHeader } from '@/components/SectionHeader';
 
 /**
  * Constants
  */
-import { statsData } from '@/constants';
+import { projectsData } from '@/constants';
 
 
 export const Projects = () => {
@@ -20,21 +25,24 @@ export const Projects = () => {
         <motion.section
             initial='hidden'
             whileInView='visible'
-            viewport={{ once: true, amount: 0.8 }}
+            viewport={{ once: true, amount: 0.3 }}
             variants={staggerContainer(0)}
-            className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 mt-20'
+            className='mt-30 scroll-mt-10'
+            id='projects'
         >
-            {statsData.map((stats, i) => (
-                <motion.div
-                    key={i}
-                    variants={fadeUp}
-                    className='border border-neutral-700 rounded-xl flex justify-center items-center flex-col py-6'
-                >
-                    <p className='text-4xl capitalize font-bold lining-nums'>{stats.number}</p>
-                    <p className='text-neutral-300'>{stats.label}</p>
-                </motion.div>
+            <SectionHeader 
+            subtitle='Projects'
+            title='My featured projects'
+            />
+            <motion.div
+                className=''
+                initial='hidden'
+                whileInView='visible'
+                viewport={{ once: true, amount: 0.3 }}
+                variants={staggerContainer(0.5)}
+            >
 
-            ))}
+            </motion.div>
         </motion.section>
     );
 }
