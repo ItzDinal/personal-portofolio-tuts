489906f21a8ab9db1c9c9295a5d4d5dfb93bad07
Dinal <sandarudinal2006@gmail.com>
2026-01-25 13:21:58 +0530

    While in IJSE

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,8 @@ import { Hero } from "@/components/Hero";
 import { Stats } from "@/components/Stats";
 import { Projects } from "@/components/Projects";
 import { About } from "@/components/About";
-
+import { Services } from "@/components/Services";
+import { Testimonials } from "@/components/Testimonials";
 export const App = () => {
   return (
     <main className="flex flex-col container mx-auto p-10 max-w-4xl lg:pr-10 lg:pl-0 lg:max-w-6xl">
@@ -23,6 +24,8 @@ export const App = () => {
       <Stats />
       <Projects />
       <About />
+      <Services />
+      <Testimonials />
     </main>
   )
 }
